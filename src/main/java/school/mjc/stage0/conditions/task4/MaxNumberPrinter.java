e25b61d51b44d3332def839d3df7b4a52f1e4cf7
farid.nasifulin <nasifulin.f@roskvartal.ru>
2023-06-09 23:43:54 +0300

    feat: solution

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first > second && first > third) {
+            System.out.println(first);
+        } else if (second > third && second > first) {
+            System.out.println(second);
+        } else if (third > second && third > first) {
+            System.out.println(third);
+        } else {
+            System.out.println(first);
+        }
+
     }
 }
